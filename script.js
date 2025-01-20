0740a66c56ae14f777782d07b0ca206d2df48a3f
Miron <mironkedich@gmail.com>
2025-01-20 22:24:17 +0300

    last version

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const SignUpBtn = document.querySelector('.title__signup-btn');
 const SignInBtn = document.querySelector('.title__signin-btn');
 const box = document.querySelector('.sign-in-up-form')
+const body = document.body
 
 SignUpBtn.addEventListener('click', function(){
     box.classList.add('active')
+    body.classList.add('active')
 });
 
 SignInBtn.addEventListener('click', function(){
     box.classList.remove('active')
+    body.classList.remove('active')
 });
